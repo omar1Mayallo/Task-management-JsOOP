06bdb27a4feda680660557cf293242020e76a455
omar mayallo <omar.mayallo4@gmail.com>
2022-11-28 18:24:37 +0200

    Build The Basic Template

---
+++ index.js
@@ -0,0 +1,10 @@
+/*
+class Task {
+  task = {id, title,description,status}
+  createTask(taskObj){}
+  getAllTasks(){}
+  deleteTask(taskId){}
+  updateTask(taskId){}
+  changeTaskStatus(taskId){} //done or not
+}
+*/
